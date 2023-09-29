565068558aa49490625efbb291a8bdcd303095e6
bertrandsimon <trandber@hotmail.com>
2023-09-29 11:24:10 +0200

    Expertise done

--- app/expertise/page.js
+++ app/expertise/page.js
@@ -1,15 +1,23 @@
+'use client'
 import Image from 'next/image'
 import ExpertiseTop from './expertiseTop'
-
+import Faq from './faq'
+import TopContent from './topContent'
 
 export default function Home() {
+
+ 
+
   return (
     <main>
 
     <div className='-mt-[120px] z-1'>
       <ExpertiseTop/>
     </div>
-   
+
+      <TopContent/>
+      
+      <Faq/>
  
    </main>
   )
