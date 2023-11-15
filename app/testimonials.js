0244162623820de3bd5f06add4ba2ba7c8458163
bertrandsimon <trandber@hotmail.com>
2023-11-15 11:01:58 +0100

    New testimonials added

--- app/testimonials.js
+++ app/testimonials.js
@@ -39,10 +39,53 @@ export default function Testimonials() {
               <div className="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 ">
                 <Image
                   className="self-start"
+                  src="/images/testimonials/jo.png"
+                  alt="LPG"
+                  height={50}
+                  width={208}
+                />
+                <figure className="mt-10 flex flex-auto flex-col justify-between ">
+                  <blockquote className="text-normal font-normal leading-8 text-gray-900 ">
+                    <p>
+                      Christophe Hausswirth consacre sa vie au service des
+                      sportifs et de la santé plus généralement ; d’abord comme
+                      athlète de niveau international, mais surtout comme
+                      chercheur avec qui j’ai eu le plaisir de travailler
+                      l’INSEP. <br />
+                      <br />
+                      Il a donc ces deux qualités rares que sont la connaissance
+                      de la pratique du haut-niveau et la rigueur scientifique
+                      mise en œuvre pour y parvenir.
+                    </p>
+                  </blockquote>
+                  <figcaption className="mt-10 flex items-center gap-x-6">
+                    <Image
+                      className="rounded-full bg-gray-50"
+                      src="/images/testimonials/man2.jpg"
+                      alt=""
+                      height={80}
+                      width={80}
+                    />
+                    <div className="text-base">
+                      <div className="font-semibold text-gray-900">
+                        Dr. Philippe Le Van
+                      </div>
+                      <div className="mt-1 text-gray-500">
+                        Directeur Médical (CMO) de Paris 2024
+                      </div>
+                    </div>
+                  </figcaption>
+                </figure>
+              </div>
+            </SwiperSlide>
+            {/* <SwiperSlide>
+              <div className="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 ">
+                <Image
+                  className="self-start"
                   src="/images/logos/lpg.png"
                   alt="LPG"
-                  height={39}
-                  width={160}
+                  height={50}
+                  width={208}
                 />
                 <figure className="mt-10 flex flex-auto flex-col justify-between ">
                   <blockquote className="text-normal font-normal leading-8 text-gray-900 ">
@@ -80,7 +123,7 @@ export default function Testimonials() {
                   </figcaption>
                 </figure>
               </div>
-            </SwiperSlide>
+            </SwiperSlide> */}
 
             <SwiperSlide>
               <div className="flex flex-col border-t border-gray-900/10 pt-10 sm:pt-16 lg:border-l lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20 lg:pr-4 xl:pr-0">
@@ -88,8 +131,8 @@ export default function Testimonials() {
                   className="self-start"
                   src="/images/logos/16.png"
                   alt="Mouratoglou"
-                  height={39}
-                  width={160}
+                  height={50}
+                  width={208}
                 />
                 <figure className="mt-10 flex flex-auto flex-col justify-between">
                   <blockquote className="text-normal font-normal leading-8 text-gray-900">
@@ -127,43 +170,42 @@ export default function Testimonials() {
             </SwiperSlide>
 
             <SwiperSlide>
-              <div className="flex flex-col border-t border-gray-900/10 pt-10 sm:pt-16 lg:border-l lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20 lg:pr-4 xl:pr-0">
+              <div className="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 ">
                 <Image
                   className="self-start"
-                  src="/images/logos/16.png"
-                  alt="Mouratoglou"
-                  height={39}
-                  width={160}
+                  src="/images/testimonials/1.png"
+                  alt="LPG"
+                  height={50}
+                  width={208}
                 />
-                <figure className="mt-10 flex flex-auto flex-col justify-between">
-                  <blockquote className="text-normal font-normal leading-8 text-gray-900">
+                <figure className="mt-10 flex flex-auto flex-col justify-between ">
+                  <blockquote className="text-normal font-normal leading-8 text-gray-900 ">
                     <p>
-                      “Un travail exceptionnel à la fois pour la mise en place
-                      de protocoles de récupération, et pour le suivi
-                      nutritionnel des athlètes.
+                      Nous travaillons aux côtés de beScored et son fondateur
+                      depuis 2020 et la facilité d&apos;exécution des recherches
+                      est remarquable. <br />
                       <br />
-                      <br />
-                      Tout ce qui touche de près ou de loin à l’optimisation de
-                      la performance est concerné. beScored est un interlocuteur
-                      attentif qui apporte toutes les réponses à mes attentes et
-                      à celles des sportifs.”
+                      Je n&apos;ai jamais vu une société pouvant être aussi
+                      précise dans la livraison des rapports, et
+                      l&apos;accompagnement des domaines d&apos;application est
+                      encore aujourd&apos;hui d&apos;un très grand soutien au
+                      quotidien pour notre société. Nous sommes partis ensemble
+                      pour plusieurs années de collaboration.
                     </p>
                   </blockquote>
                   <figcaption className="mt-10 flex items-center gap-x-6">
                     <Image
                       className="rounded-full bg-gray-50"
-                      src="/images/moura.jpg"
+                      src="/images/testimonials/man.jpg"
                       alt=""
                       height={80}
                       width={80}
                     />
                     <div className="text-base">
                       <div className="font-semibold text-gray-900">
-                        Patrick Mouratoglou
+                        Sammy Gharieni
                       </div>
-                      <div className="mt-1 text-gray-500">
-                        Founder Mouratoglou Tennis Academy
-                      </div>
+                      <div className="mt-1 text-gray-500">Gharieni group</div>
                     </div>
                   </figcaption>
                 </figure>
